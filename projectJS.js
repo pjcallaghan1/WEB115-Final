a2834c7d92db5a8eb690e714f2d547fbee4962ab
pjcallaghan1 <130389389+pjcallaghan1@users.noreply.github.com>
2023-11-24 12:16:48 -0500

    Add files via upload

---
+++ projectJS.js
@@ -0,0 +1,8 @@
+//Prints Weekly Planner
+btnPrint = document.getElementById("print");
+
+//Downloads Weekly Planner
+btnDownload = document.getElementById("download");
+
+//Submits Form
+btnSubmit = document.getElementById("submit");
